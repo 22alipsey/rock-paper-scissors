93c420b00e43575c1e868cc96d9a4e7acc034c47
22alipsey <80347394+22alipsey@users.noreply.github.com>
2021-03-09 09:46:24 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let Hand = 0
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (Hand == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (Hand == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
